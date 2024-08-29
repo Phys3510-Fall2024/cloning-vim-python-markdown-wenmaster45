7b2722ff195fd0a37a9cb28bfc52ebc370a163d1
Wendell <Wenmaster45@gmail.com>
2024-08-29 10:35:24 -0500

    completed hello-world assignment

---
+++ hello-world.py
@@ -0,0 +1,7 @@
+# This program asks the user for their name and interest and prints the result to screen 
+
+user_name = input("What is your name? ") 
+
+user_interest = input("What are you interested in? ")
+
+print("My name is " + user_name + ", and I like " + user_interest + ".")
